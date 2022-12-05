fb105322f452976e70493023af4427947e3f6f18
Sahil Kamath <sahilkamath0108@gmail.com>
2022-12-05 20:04:40 +0530

    add personal chats and chat rooms

--- app.js
+++ app.js
@@ -27,11 +27,20 @@ app.listen(3000 , ()=>{
 
 io.on("connect",(socket)=>{
   console.log(socket.id)
-  socket.on("sendMessage", message=>{
-    console.log(message+ " \"text by user\" "+socket.id)
+  socket.on("sendMessage", (message, room)=>{
+    // console.log(message+ " \"text by user\" "+socket.id)
     // io.emit('receiveMessage', message)
-    socket.broadcast.emit('receiveMessage', message)
-  }) 
+    if(!room){
+    socket.broadcast.emit('receiveMessage', (message+"  -->(by user)"+socket.id+" to all" ))
+    }else{
+      socket.to(room).emit('receiveMessage', (message+"  -->(by user)"+socket.id+" to you" ))
+    }
+  })
+  
+  socket.on("joinRoom", room => {
+    socket.join(room)
+    console.log(socket.id+" joined room "+room)
+  })
 })
 
 
--- clientSide.js
+++ clientSide.js
@@ -2,9 +2,10 @@ const io = require("socket.io-client")
 
 const socket = io("http://localhost:5050")
 
-socket.on("connect", (message) =>{
+socket.on("connect", (message,room) =>{
     console.log("Connected with "+socket.id)
-    socket.emit("sendMessage", message)
+    socket.emit("sendMessage", message, room)     //message to all or personal
+    socket.emit("joinRoom", room)          //join a particular room
 })
 
 socket.on("receiveMessage", message => {
