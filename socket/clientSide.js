b20d1e11d3a62341518a36bebb3e825704682c8e
Sahil Kamath <sahilkamath0108@gmail.com>
2022-12-06 22:34:45 +0530

    Provide loose Auth for users.

--- socket/clientSide.js
+++ socket/clientSide.js
@@ -2,7 +2,7 @@ const io = require("socket.io-client")
 
 const socket = io("http://localhost:5050")
 
-socket.on("connect", (message,room) =>{
+socket.on("connection", (message,room) =>{
     console.log("Connected with "+socket.id)
     socket.emit("sendMessage", message, room)     //message to all or personal
     socket.emit("joinRoom", room)          //join a particular room
--- socket/serverSide.js
+++ socket/serverSide.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
+const jwt = require("jsonwebtoken");
+const UserSchema = require('../models/userSchema');
 const {instrument} = require("@socket.io/admin-ui")
 const io = require("socket.io")(5050,{
     cors: {
@@ -5,24 +8,39 @@ const io = require("socket.io")(5050,{
     },
 });
 require("./clientSide")
+var noToken = 0;
 
+io.use(async (socket,next)=>{
+  
+  const token = socket.handshake.headers.authorization;
+  if(!token && noToken===0){
+    next()
+    noToken++
+  }else if(!token && noToken===1){
+    console.log("No Token Provided")
+  }else{
+  const decode = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+  const user = await UserSchema.findById({_id : decode._id})
+  socket.username=user.username
+  next()
+  }
+})
 
-
-io.on("connect",(socket)=>{
-    console.log(socket.id)
+io.on("connection",(socket)=>{
+    console.log(socket.username)
     socket.on("sendMessage", (message, room)=>{
       // console.log(message+ " \"text by user\" "+socket.id)
       // io.emit('receiveMessage', message)
       if(!room){
-      socket.broadcast.emit('receiveMessage', (message+"  -->(by user)"+socket.id+" to all" ))
+      socket.broadcast.emit('receiveMessage', (message+"  -->(by user)"+socket.username+" to all" ))
       }else{
-        socket.to(room).emit('receiveMessage', (message+"  -->(by user)"+socket.id+" to you" ))
+        socket.to(room).emit('receiveMessage', (message+"  -->(by user)"+socket.username+" to you" ))
       }
     })
     
     socket.on("joinRoom", room => {
       socket.join(room)
-      console.log(socket.id+" joined room "+room)
+      console.log(socket.username+" joined room "+room)
     })
   })
 
